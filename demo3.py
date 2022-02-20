bb04a5708afae1f07fb33fa014860fd14b364e38
thangquangloi21 <83813835+thangquangloi21@users.noreply.github.com>
2022-02-20 15:15:36 +0700

    Pack()

---
+++ demo3.py
@@ -0,0 +1,5 @@
+from tkinter import *
+win = Tk()
+# Label(win,text="Hello toi la thang quang loi",bg='red').pack(side=BOTTOM,pady= 50,ipadx=50,ipady=50)
+Label(win,text="Hello toi la thang quang loi",bg='red').pack(fill=  X,ipady=50)
+win.mainloop()
